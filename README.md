4efe856a749d940a890a2f2df98e890edeff8e4f
Mikita Vorobyov <olfb_c@ukr.net>
2016-01-30 22:20:56 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# cPanel-API-python-script-Getting-info-about-accounts
-cPanel API python script. Getting info about accounts.
+#### cPanel API python script. Getting info about accounts.
+
+If you have a set of cPanels  and you need to know how many accounts in each,
+you can use this python script.
+
+Output information about cPanels  will write on `out` file.
+
+If you need to get some different info from each cPanel, you can easy change the code.
+(see comments in code)
+
+In the head of script you need to change this data:
+
+	#=============================================================
+	admin_login="your_login"
+
+	admin_password="your_password"
+
+	server_list=[# list of your cpanels
+
+		    'cpanel_1.your_domain.com',
+
+		    'cpanel_2.your_domain.com',
+
+		    'cpanel_3.your_domain.com',
+
+		    ]
+
+	#=============================================================
+
+In this variant all cPanels have the same login and password.
+
+#### Requirements
+	Python 2.7.10
+	pip install pycpanel==0.1.5
+
+
+#### Usage 
+	python acc_list.py
+
+**Note**: this repository contains a set of scripts for create/delete accounts and ip addresses.
+`https://github.com/baloon11/cPanel-API-python-scripts`
+
+
+
+
+
+
---
+++ acc_list.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+import os
+import pycpanel
+
+#=============================================================
+admin_login="your_login"
+admin_password="your_password"
+server_list=[# list of your cPanels
+            'cpanel_1.your_domain.com',
+            'cpanel_2.your_domain.com',
+            'cpanel_3.your_domain.com',
+            ]
+#=============================================================
+
+if os.path.exists('out'):
+    os.remove('out')
+
+for server_name in server_list:
+    out=open('out','a')
+    server = pycpanel.conn(hostname=server_name,
+                           username=admin_login, 
+                           password=admin_password) #Basic user/password authentication
+    #server.api('listaccts')['acct'] -list of dict`s (each dict - info about separate account)
+    account_list=server.api('listaccts')['acct']
+    out.write("%s %s %s"%(server_name,len(account_list),"accouts\n"))
+    print 'proccesed server ',server_name
+    # for account in account_list: # if you need something else info about accout (for example, domain)
+    #     out.write(account['domain']+"\n")# uncomment this loop
+#    out.write("======================================================\n")
+    out.close()
